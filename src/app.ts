6ee63e73c83d6b35b162deaca6f6adc4ebe6891b
Alex Fagan <s00267631@atu.ie>
2026-09-23 10:13:39 +0100

    up to the end fixing stuff

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,35 @@
 import express, {Application, Request, Response} from "express" ;
 import carRoutes from './routes/cars';
+import { env } from "./config/env";
+import { connectDB } from "./config/database";
 
-
-const PORT = process.env.PORT || 3001;
+const port = env.port
 
 const app: Application = express();
+  
 app.use(express.json());
 app.use('/api/v1/cars', carRoutes);
+app.use((req,_res,next) => {
+    console.log(`Request received: ${req.method} ${req.originalUrl}`);
+    next();
+});
 app.get("/ping", async (_req : Request, res: Response) => {
     res.json({
     message: "hello from da dwifta"
     });
 });
 
-app.listen(PORT, () => {
-    console.log("Server is running on port", PORT);
+app.listen(port, () => {
+    console.log("Server is running on port", port);
     });
+    const startServer = async () => {
+  await connectDB();
+
+  app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
+  };
+
+startServer();
+    
+//8IS3HE76dW2z6C1i s00267631_db_user
