f39ee1ff5e423faf82fd1ef2b4fc6a95e21945f0
Alex Fagan <s00267631@atu.ie>
2026-09-23 09:41:07 +0100

    Worksheet2

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,12 @@
 import express, {Application, Request, Response} from "express" ;
+import carRoutes from './routes/cars';
+
 
 const PORT = process.env.PORT || 3001;
 
 const app: Application = express();
+app.use(express.json());
+app.use('/api/v1/cars', carRoutes);
 app.get("/ping", async (_req : Request, res: Response) => {
     res.json({
     message: "hello from da dwifta"
